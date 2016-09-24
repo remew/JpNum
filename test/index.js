5463e74486a10a1d4f6accd596f741df2ef4f4bc
remew <foolishremew@gmail.com>
2016-09-24 22:32:54 +0900

    Add big number test

--- test/index.js
+++ test/index.js
@@ -3,13 +3,16 @@
 const assert = require('assert');
 const {toArabic} = require('../');
 
-describe('Japanese Numbers', () => {
-    describe('toArabic', () => {
+describe('JapaneseNumbers', () => {
+    describe('#toArabic', () => {
         it('should be return 42 when given 四十二', () => {
             assert.equal(toArabic('四十二'), '42');
         });
         it('should be return 100 when given 百', () => {
             assert.equal(toArabic('百'), '100');
+        });
+        it('should be return 6000053124710 when given 六兆五千三百十二万四千七百十', () => {
+            assert.equal(toArabic('六兆五千三百十二万四千七百十'), '6000053124710');
         });
     });
 });
