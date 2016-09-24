39804778eee0f01c7b918d7a73b1b662b2cec447
remew <foolishremew@gmail.com>
2016-09-24 21:01:44 +0900

    Implement simple biginteger

---
+++ src/calc.js
@@ -0,0 +1,48 @@
+'use strict';
+
+function add(a, b) {
+    a = [...a].reverse();
+    b = [...b].reverse();
+    const ans = [];
+    const len = a.length < b.length ? b.length : a.length;
+    let up = 0;
+    for (let i = 0; i < len; i++) {
+        const tmp = parseInt(a[i] || 0) + parseInt(b[i] || 0) + up;
+        up = Math.floor(tmp / 10);
+        ans.push(tmp % 10);
+    }
+    if (up !== 0) {
+        ans.push(up);
+    }
+    return ans.reverse().join('');
+}
+
+function mul(a, b) {
+    a = [...a].reverse();
+    b = [...b].reverse();
+    const [outer, inner] = a.length < b.length ? [a, b] : [b, a];
+    const oLen = outer.length;
+    const iLen = inner.length;
+    const ans = [];
+    for (let i = 0; i < oLen; i++) {
+        const num = parseInt(outer[i]);
+        const line = Array.from({length: i}, _ => '0');
+        let carry = 0;
+        for (let j = 0; j < iLen; j++) {
+            const tmpAns = num * parseInt(inner[j]) + carry;
+            carry = Math.floor(tmpAns / 10);
+            line.push(tmpAns % 10);
+        }
+        if (carry !== 0) {
+            line.push(carry);
+        }
+        ans.push(line.reverse().join(''));
+    }
+    return ans.reduce((result, line) => add(result, line));
+}
+
+module.exports = {
+    add,
+    mul,
+};
+
---
+++ test/calcTest.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const assert = require('assert');
+const {add, mul} = require('../src/calc');
+
+describe('calc.js', () => {
+    describe('add', () => {
+        it('should be return 42 when given 10 and 32', () => {
+            assert.equal(add('10', '32'), '42');
+        });
+        it('should be return 1001 when given 1000 and 1', () => {
+            assert.equal(add('1000', '1'), '1001');
+        });
+        it('should be return 100 when given 99 and 1', () => {
+            assert.equal(add('99', '1'), '100');
+        });
+    });
+    describe('mul', () => {
+        it('should be return 42 when given 2 and 21', () => {
+            assert.equal(mul('2', '21'), '42');
+        });
+        it('should be return 121 when given 11 and 11', () => {
+            assert.equal(mul('11', '11'), '121');
+        });
+        it('should be return 32111247 when given 9793 and 3279', () => {
+            assert.equal(mul('9793', '3279'), '32111247');
+        });
+        it('should be return 33764558458674967278141271247 when given 141592653589793 and 238462643383279', () => {
+            assert.equal(mul('141592653589793', '238462643383279'), '33764558458674967278141271247');
+        });
+    });
+});
