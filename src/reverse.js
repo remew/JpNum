5a3e0ed53037feed4eae698c78160ee32c360990
remew <foolishremew@gmail.com>
2016-09-26 11:32:12 +0900

    add jsdoc to reverse

--- src/reverse.js
+++ src/reverse.js
@@ -1,5 +1,11 @@
 'use strict';
 
+/**
+ * Reverse the given array.
+ * Difference of Array#reverse is Array#reverse break itself but this function create new Array instance
+ * @param {Array} array
+ * @return {Array} reversed array
+ */
 module.exports = function reverse(array) {
     return Array.from(array).reverse();
 };
